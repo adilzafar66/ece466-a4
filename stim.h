303d3120b87b4cb689df072b439438e52bd8db87
Adil <adilzafar66@gmail.com>
2023-07-22 02:40:46 -0700

    first commit

---
+++ stim.h
@@ -0,0 +1,24 @@
+#include "systemc.h"
+
+SC_MODULE(stim)
+{
+  sc_out<sc_fixed <16, 3, SC_TRN, SC_WRAP>> x;
+  sc_out<bool> reset;
+  sc_in_clk clock;
+
+  void stimgen() {
+    reset.write(true);
+    x.write(0);
+    wait();
+    reset.write(false);
+    x.write(1);
+    wait();
+    x.write(0);
+    wait();
+  }
+
+  SC_CTOR(stim) {
+    SC_CTHREAD(stimgen, clock.pos());
+  }
+
+};
